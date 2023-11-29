76d3ec27ebb00ed3575c25fd0c0c00dd427de83f
Marton Csizmazia <martoncsizmazia@voormedia.com>
2023-11-29 09:47:53 +0100

    full cv placed in flexbox and centered horiz/vert.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
       <motion.div
-          className="App"
+          className="motion"
           initial={{opacity:0}}
           animate={{opacity:1}}
           transition={{ duration: 0.3 }}
